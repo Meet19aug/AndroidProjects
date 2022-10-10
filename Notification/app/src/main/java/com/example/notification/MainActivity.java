d1070d1d5f6221933b339884229a5b11fefd9610
MEET PATEL <meetpatel19aug@gmail.com>
2022-10-11 01:04:38 +0530

    SensorChange Color[partially] and notification Working[completed].

--- Notification/app/src/main/java/com/example/notification/MainActivity.java
+++ Notification/app/src/main/java/com/example/notification/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.notification;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
-import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -22,32 +20,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         send=findViewById(R.id.send);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-        {
-            NotificationChannel channel=new NotificationChannel("My Notification","My notification", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager notificationManager=getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-
-        }
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+//        {
+//            NotificationChannel channel=new NotificationChannel("My Notification","My notification", NotificationManager.IMPORTANCE_DEFAULT);
+//            NotificationManager notificationManager=getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//
+//        }
         send.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.S)
             @Override
             public void onClick(View v) {
+//                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"My Notification");
+//                builder.setContentTitle("My title");
+//                builder.setContentText("This is notification demo");
+//                builder.setSmallIcon(R.drawable.notification);
+//                builder.setAutoCancel(true);
                 NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"My Notification");
-                builder.setContentTitle("My title");
-                builder.setContentText("This is notification demo");
+
                 builder.setSmallIcon(R.drawable.notification);
+                builder.setContentTitle("Notification Example");
+                builder.setContentText("This is a notification message");
                 builder.setAutoCancel(true);
+                builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                Intent notificationIntent = new Intent(MainActivity.this, Notification.class);
+                notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                notificationIntent.putExtra("message", "This is a notification message");
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, notificationIntent,
+                        PendingIntent.FLAG_MUTABLE);
+                builder.setContentIntent(pendingIntent);
 
-
-
-//                Intent notificationIntent = new Intent(MainActivity.this, Notification.class);
-//                notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                notificationIntent.putExtra("message", "This is a notification message");
-//
-//                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, notificationIntent,
-//                        PendingIntent.FLAG_UPDATE_CURRENT);
-//                builder.setContentIntent(pendingIntent);
-                NotificationManagerCompat managerCompat= NotificationManagerCompat.from(MainActivity.this);
+                NotificationManager managerCompat= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 managerCompat.notify(1,builder.build());
 
             }
---
+++ SensorChangeColor/app/src/main/java/com/example/sensorchangecolor/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.sensorchangecolor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private float mAccel;
+    private float mAccelCurrent;
+    private float mAccelLast;
+    public String colorArray[] = {"#9dc4c3","#a4b6c0","#ab851f","#ab851f","#ab1f96"};
+    public static int c=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Objects.requireNonNull(mSensorManager).registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        mAccel = 10f;
+        mAccelCurrent = SensorManager.GRAVITY_EARTH;
+        mAccelLast = SensorManager.GRAVITY_EARTH;
+
+
+    }
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            mAccelLast = mAccelCurrent;
+            mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+            float delta = mAccelCurrent - mAccelLast;
+            mAccel = mAccel * 0.9f + delta;
+            if (mAccel > 12) {
+                Toast.makeText(getApplicationContext(), "Shake event detected", Toast.LENGTH_SHORT).show();
+                getWindow().getDecorView().setBackgroundColor(Color.parseColor(colorArray[c]));
+                if(c>4){
+                    c=0;
+                }else{
+                    c++;
+                }
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
+    }
+}
